17bbc350ed5f7a8c29463bda84e846c20bdd8caa
sws4sog23 <sws4sog23@gmail.com>
2024-09-22 22:01:02 +0300

    Add files via upload

--- modul_6_1.py
+++ modul_6_1.py
@@ -1,27 +1,32 @@
 class Animal:
     alive = True
     fed = False
+
     def __init__(self, name):
         self.name = name
+
 
 class Plant:
     edible = False
+
     def __init__(self, name):
         self.name = name
 
+
 class Mammal(Animal):
     def eat(self, food):
-        self.food = Plant.edible
+        self.food = Plant
         if food.edible == True:
             print(f"{self.name} съел {food.name}")
             self.fed = True
         elif food.edible == False:
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
+
 
 class Predator(Animal):
     def eat(self, food):
-        self.food = Plant.edible
+        self.food = Plant
         if food.edible == True:
             print(f"{self.name} съел {food.name}")
             self.fed = True
@@ -29,10 +34,14 @@ class Predator(Animal):
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
 
+
 class Flower(Plant):
     pass
+
+
 class Fruit(Plant):
     edible = True
+
 
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
