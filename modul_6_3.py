c2156cca29796d83df9045e72d54cd9c9c41f97f
sws4sog23 <sws4sog23@gmail.com>
2024-09-26 23:29:33 +0300

    Add files via upload

---
+++ modul_6_3.py
@@ -0,0 +1,32 @@
+class Horse:
+    x_distance = 0
+    sound = 'Frrr'
+    def run(self, dx):
+        self.x_distance += dx
+
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
+    def fly(self, dy):
+        self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+    def move(self, dx, dy):
+        self.x_distance += dx
+        self.y_distance += dy
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        self.sound = Eagle.sound
+        print(self.sound)
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
